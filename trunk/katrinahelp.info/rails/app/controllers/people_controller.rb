69b3aaebb9d04fae54ebf13cbf11e5d27b4df5a7
cilibrar <cilibrar@ff39199c-abed-0310-a7e6-912925c56e22>
2005-09-10 13:17:55 +0000

    Fixing stuff.

--- trunk/katrinahelp.info/rails/app/controllers/people_controller.rb
+++ trunk/katrinahelp.info/rails/app/controllers/people_controller.rb
@@ -1,7 +1,7 @@
 class PeopleController < ApplicationController
   def index
-    list
-    render :action => 'list'
+    search
+    render :action => 'search'
   end
 
   def dbesc(str)
@@ -10,15 +10,35 @@ class PeopleController < ApplicationController
     str.gsub!(/[']/,'\\\\\'')
     "'#{str}'"
   end
-  def list
-    @person_pages, @people = paginate :person, :per_page => 10
+
+  #def list
+  #  @person_pages, @people = paginate :person, :per_page => 10
+  #end
+
+  def whereclause(searchterm)
+      "MATCH(searchstuff) AGAINST(#{dbesc(searchterm)})"
+  end
+
+  def sqlstmt(searchterm)
+      "SELECT * FROM people WHERE #{whereclause(searchterm)}";
   end
 
   def search
     searchterm = @params['searchterm']
     if searchterm
-      @people = Person.find_by_sql("SELECT * FROM people WHERE MATCH(searchstuff) AGAINST(#{dbesc(searchterm)});");
-      @person_pages, @people = paginate :person, :per_page => 10
+      numperpage = 20
+      paginate_from_sql(Person, sqlstmt(searchterm), Person.count(whereclause(searchterm)), numperpage)
+#      @person_pages, @people = paginate :person, :per_page => 10
+    else
+      @people = nil
+    end
+  end
+
+  def paginate_from_sql(model, sql, total, per_page)
+    @person_pages = Paginator.new self, total, per_page, @params['page']
+    if @person_pages
+      @people = model.find_by_sql(sql + " LIMIT #{per_page} " +
+                              "OFFSET #{@person_pages.current.to_sql[1]}")
     else
       @people = nil
     end
@@ -28,36 +48,36 @@ class PeopleController < ApplicationController
     @person = Person.find(params[:id])
   end
 
-  def new
-    @person = Person.new
-  end
+#  def new
+#    @person = Person.new
+#  end
 
-  def create
-    @person = Person.new(params[:person])
-    if @person.save
-      flash[:notice] = 'Person was successfully created.'
-      redirect_to :action => 'list'
-    else
-      render :action => 'new'
-    end
-  end
+#  def create
+#    @person = Person.new(params[:person])
+#    if @person.save
+#      flash[:notice] = 'Person was successfully created.'
+#      redirect_to :action => 'list'
+#    else
+#      render :action => 'new'
+#    end
+#  end
 
-  def edit
-    @person = Person.find(params[:id])
-  end
+#  def edit
+#    @person = Person.find(params[:id])
+#  end
 
-  def update
-    @person = Person.find(params[:id])
-    if @person.update_attributes(params[:person])
-      flash[:notice] = 'Person was successfully updated.'
-      redirect_to :action => 'show', :id => @person
-    else
-      render :action => 'edit'
-    end
-  end
+#  def update
+#    @person = Person.find(params[:id])
+#    if @person.update_attributes(params[:person])
+#      flash[:notice] = 'Person was successfully updated.'
+#      redirect_to :action => 'show', :id => @person
+#    else
+#      render :action => 'edit'
+#    end
+#  end
 
-  def destroy
-    Person.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  end
+#  def destroy
+#    Person.find(params[:id]).destroy
+#    redirect_to :action => 'list'
+#  end
 end
