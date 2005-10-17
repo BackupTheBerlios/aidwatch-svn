26bfc52c239da7f5fb4bb1b5da2e940d9884bd15
alcruz <alcruz@ff39199c-abed-0310-a7e6-912925c56e22>
2005-10-17 10:45:45 +0000

    slight bit of styling; removed destroy and edit links

--- trunk/katrinahelp.info/smswiki/app/controllers/wiki_messages_controller.rb
+++ trunk/katrinahelp.info/smswiki/app/controllers/wiki_messages_controller.rb
@@ -12,36 +12,36 @@ class WikiMessagesController < ApplicationController
     @wiki_message = WikiMessage.find(params[:id])
   end
 
-  def new
-    @wiki_message = WikiMessage.new
-  end
-
-  def create
-    @wiki_message = WikiMessage.new(params[:wiki_message])
-    if @wiki_message.save
-      flash[:notice] = 'WikiMessage was successfully created.'
-      redirect_to :action => 'list'
-    else
-      render :action => 'new'
-    end
-  end
-
-  def edit
-    @wiki_message = WikiMessage.find(params[:id])
-  end
-
-  def update
-    @wiki_message = WikiMessage.find(params[:id])
-    if @wiki_message.update_attributes(params[:wiki_message])
-      flash[:notice] = 'WikiMessage was successfully updated.'
-      redirect_to :action => 'show', :id => @wiki_message
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    WikiMessage.find(params[:id]).destroy
-    redirect_to :action => 'list'
-  end
+#  def new
+#    @wiki_message = WikiMessage.new
+#  end
+#
+#  def create
+#    @wiki_message = WikiMessage.new(params[:wiki_message])
+#    if @wiki_message.save
+#      flash[:notice] = 'WikiMessage was successfully created.'
+#      redirect_to :action => 'list'
+#    else
+#      render :action => 'new'
+#    end
+#  end
+#
+#  def edit
+#    @wiki_message = WikiMessage.find(params[:id])
+#  end
+#
+#  def update
+#    @wiki_message = WikiMessage.find(params[:id])
+#    if @wiki_message.update_attributes(params[:wiki_message])
+#      flash[:notice] = 'WikiMessage was successfully updated.'
+#      redirect_to :action => 'show', :id => @wiki_message
+#    else
+#      render :action => 'edit'
+#    end
+#  end
+#
+#  def destroy
+#    WikiMessage.find(params[:id]).destroy
+#    redirect_to :action => 'list'
+#  end
 end
