dc88731d969217c2bb89500e5ce50762018dc710
cilibrar <cilibrar@ff39199c-abed-0310-a7e6-912925c56e22>
2005-10-19 21:19:48 +0000

    Increased sizes of some fields....

--- trunk/katrinahelp.info/smswiki/db/wikimessage.sql
+++ trunk/katrinahelp.info/smswiki/db/wikimessage.sql
@@ -1,8 +1,8 @@
 CREATE TABLE wiki_messages (
   `id` int(11) NOT NULL auto_increment,
-  mar VARCHAR(16383),
-  subj VARCHAR(255),
-  body VARCHAR(1024),
+  mar BLOB,
+  subj VARCHAR(1024),
+  body BLOB,
   created_at datetime,
   modified_at datetime,
   PRIMARY KEY  (`id`)
